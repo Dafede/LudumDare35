ce938518a168e206aae6a4e4a0a55e251b5904fa
Kensousen <dafede333@gmail.com>
2016-04-16 17:19:26 +0200

    movement

---
+++ Assets/Scripts/MainCamera.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainCamera : MonoBehaviour {
+
+	float hSpeed = 0.25f;
+	float vSpeed = 0.25f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+		float h = hSpeed * Input.GetAxis ("Mouse X");
+		float v = hSpeed * Input.GetAxis ("Mouse Y");
+		Debug.Log (v);
+		this.transform.Translate (h,0,v);
+		Vector3 auxPos = this.transform.position;
+		auxPos.y = 0;
+		this.transform.position = auxPos;
+
+	}
+}
---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour {
+
+	Vector3 pos;
+	float spacing = 0.1f;
+	bool dash=false;
+	KeyCode currentKeyPressed;
+	Vector3 currentPosition;
+	private float startTime;
+	float speed = 50.0F;
+	private float journeyLength;
+	 Vector3 startMarker;
+	Vector3 endMarker;
+		
+	// Use this for initialization
+	void Start () {
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+		if (Input.GetKey (KeyCode.W)) {
+			currentKeyPressed=KeyCode.W;
+			pos = transform.position;
+			pos.z += spacing;
+			pos.x += spacing;
+			//transform.LookAt (Vector3.Lerp(transform.position, pos, fracJourney));
+			transform.position = pos;
+
+		}
+		if (Input.GetKey (KeyCode.S)) {
+			currentKeyPressed=KeyCode.S;
+			pos = transform.position;
+			pos.z -= spacing;
+			pos.x -= spacing;
+			transform.position = pos;
+		}
+		if (Input.GetKey (KeyCode.A)) {
+			currentKeyPressed=KeyCode.A;
+			pos = transform.position;
+			pos.x -= spacing;
+			pos.z += spacing;
+			transform.position = pos;
+		}
+		if (Input.GetKey (KeyCode.D)) {
+			currentKeyPressed=KeyCode.D;
+			pos = transform.position;
+			pos.x += spacing;
+			pos.z -= spacing;
+			transform.position = pos;
+		}
+				
+		if (dash) {
+			float distCovered = (Time.time - startTime) * speed;
+			float fracJourney = distCovered / journeyLength;
+			if (fracJourney >= 0.9)
+				dash = false;
+			transform.position = Vector3.Lerp(startMarker, endMarker, fracJourney);
+		}
+		if (Input.GetKeyDown(KeyCode.Space)) {
+			currentPosition=this.transform.position;
+			dash = true;
+			startTime = Time.time;
+			endMarker= this.transform.position;
+			Vector3 auxEnd = endMarker;
+			auxEnd.x += 15;
+			endMarker = auxEnd;
+			startMarker = this.transform.position;
+			journeyLength = Vector3.Distance(startMarker,endMarker);
+		}
+	}
+}
